a4d967b09c48d25b4000556aeece3847a9b58dd9
howard611 <howard.tam.95@gmail.com>
2016-10-22 22:33:46 -0400

    Event driven programming

--- main.cpp
+++ main.cpp
@@ -32,7 +32,7 @@ SDL_Window * gWindow = NULL;
 SDL_Surface * gScreenSurface = NULL;
 
 // The image we will load and show on the screen
-SDL_Surface * gHelloWorld = NULL;
+SDL_Surface * gXOut = NULL;
 
 // Notice that these are pointers to SDL surfaces. The reason is that 1) we'll be dynamically allocating memory to load images and 2) it's better to reference an image by memory location.
 // Also, always remember to initialize your pointers. We set them to NULL immediately when declaring them.
@@ -66,10 +66,10 @@ bool loadMedia() {
     bool success = true;
     
     // Load splash image
-    gHelloWorld = SDL_LoadBMP( "02_getting_an_image_on_the_screen/hello_world.bmp" );
+    gXOut = SDL_LoadBMP( "03_event_driven_programming/x.bmp" );
     
-    if ( gHelloWorld == NULL ) {
-        printf("Unable to load image %s! SDL_Error: %s\n", "02_getting_an_image_on_the_screen/hello_world.bmp", SDL_GetError() );
+    if ( gXOut == NULL ) {
+        printf("Unable to load image %s! SDL_Error: %s\n", "03_event_driven_programming/x.bmp", SDL_GetError() );
         success = false;
     }
     
@@ -85,7 +85,7 @@ void close() {
     
     // Deallocate surface
     SDL_FreeSurface(gScreenSurface);
-    gHelloWorld = NULL;
+    gXOut = NULL;
     
     // Destroy window
     SDL_DestroyWindow(gWindow);
@@ -106,22 +106,38 @@ int main (int argc, char* args[]) {
         if ( !loadMedia() ) {
             printf("Failed to load media!\n");
         } else {
-            // Apply the image
-            SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
+            // Main loop flag
+            bool quit = false;
             
-//  What blitting does is take a source surface and stamps a copy of it onto the destination surface. The first argument of SDL_BlitSurface is the source image. The third argument is the destination. We'll worry about the 2nd and 4th arguments in future tutorials.
-
+            // Event handler
+            SDL_Event e;
+            
+            // While application is running
+            while (!quit) {
+                // Handle events on queue
+                while ( SDL_PollEvent( &e ) != 0 ) {
+                    // User requests quit
+                    if ( e.type == SDL_QUIT ) {
+                        quit = true;
+                    }
+                }
+                
+//                SDL_PollEvent will keep taking events off the queue until it is empty. When the queue is empty, SDL_PollEvent will return 0. So what this piece of code does is keep polling events off the event queue until it's empty. If an event from the event queue is an SDL_QUIT event (which is the event when the user Xs out the window), we set the quit flag to true so we can exit the application.
+            }
+            
+            // Apply the image
+            SDL_BlitSurface(gXOut, NULL, gScreenSurface, NULL);
+            
+            //  What blitting does is take a source surface and stamps a copy of it onto the destination surface. The first argument of SDL_BlitSurface is the source image. The third argument is the destination. We'll worry about the 2nd and 4th arguments in future tutorials.
+            
             // Update the surface
             SDL_UpdateWindowSurface(gWindow);
             
-            // Wait 2 seconds
-            SDL_Delay( 5000 );
-            
-//  After drawing everything on the screen that we want to show for this frame we have to update the screen using SDL_UpdateWindowSurface. See when you draw to the screen, you are not typically drawing to the image on the screen you see. By default, most rendering systems out there are double buffered. These two buffers are the front and back buffer.
-//                
-//  When you make draw calls like SDL_BlitSurface, you render to the back buffer. What you see on the screen is the front buffer. The reason we do this is because most frames require drawing multiple objects to the screen. If we only had a front buffer, we would be able to see the frame as things are being drawn to it which means we would see unfinished frames. So what we do is draw everything to the back buffer first and once we're done we swap the back and front buffer so now the user can see the finished frame.
-//                    
-//  This also means that you don't call SDL_UpdateWindowSurface after every blit, only after all the blits for the current frame are done.
+            //  After drawing everything on the screen that we want to show for this frame we have to update the screen using SDL_UpdateWindowSurface. See when you draw to the screen, you are not typically drawing to the image on the screen you see. By default, most rendering systems out there are double buffered. These two buffers are the front and back buffer.
+            //
+            //  When you make draw calls like SDL_BlitSurface, you render to the back buffer. What you see on the screen is the front buffer. The reason we do this is because most frames require drawing multiple objects to the screen. If we only had a front buffer, we would be able to see the frame as things are being drawn to it which means we would see unfinished frames. So what we do is draw everything to the back buffer first and once we're done we swap the back and front buffer so now the user can see the finished frame.
+            //
+            //  This also means that you don't call SDL_UpdateWindowSurface after every blit, only after all the blits for the current frame are done.
         }
     }
     
